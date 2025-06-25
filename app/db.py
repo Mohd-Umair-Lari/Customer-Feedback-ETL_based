b6afc121f98b2f3c080332105d610adbe618c298
Umair Lari <umairlari0786@gmail.com>
2025-06-25 22:10:04 +0530

    test case added

--- app/db.py
+++ app/db.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, Table
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./products.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DB_URL = "sqlite:///./products.db"
+engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
